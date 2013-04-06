2d39200ec89c7456fc14eb734b9c075151e7eeca
reecek <reecekol@gmail.com>
2013-04-06 10:04:13 -0400

    added lib

---
+++ mathiply.js
@@ -0,0 +1,43 @@
+window.mathiply=(function()
+{
+
+ var mathiply={
+ 
+  add:function(a,b)
+  {
+    return(a+b); 
+  },
+  
+  sub:function(a,b)
+  {
+  
+   return(a-b);
+  },
+  
+  mul:function(a,b)
+  {
+  
+   return(a*b); 
+  },
+ 
+  
+  div:function(a,b)
+  {
+  
+    return(a/b);
+  },
+ 
+ 
+  mod:function(a,b)
+  {
+    return(a%b);
+  }
+ 
+ 
+ }
+ 
+ 
+ return mathiply;
+ alert("hello world");
+
+})();
